76d7c2764e66edea32dd5deea926099dee47981c
George Chanturia <g-o-g-i-@mail.ru>
2020-06-10 22:08:05 +0300

    add methods, create comparator class

---
+++ src/main/java/domain/EmployerComparator.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.util.Comparator;
+
+public class EmployerComparator implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return  o1.getAge()-o2.getAge();
+    }
+
+
+}
