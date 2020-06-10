bd8e7d19c078fded763820476a97cf00fbc1f7b5
George Chanturia <g-o-g-i-@mail.ru>
2020-06-10 21:52:20 +0300

    add methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,43 @@
 import domain.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
+
         CleanWasher cleanWasher = new CleanWasher("Айгуль", 24, 60, 100);
 
-        Employer cashier = new Cashier("Галя", 54, 70, 200);
-        Employer cashier1 = new Cashier("Оля", 59, 69, 190);
-        Employer cashier2 = new Cashier("Наташа", 54, 75, 210);
+        Employee cashier = new Cashier("Галя", 54, 70, 200);
+        Employee cashier1 = new Cashier("Оля", 59, 69, 190);
+        Employee cashier2 = new Cashier("Наташа", 54, 75, 210);
 
-        Employer sMMSpecialist = new SMMSpecialist("Костя", 24, 80, 350);
-        Employer sMMSpecialist1 = new SMMSpecialist("Петя", 29, 76, 330);
-        Employer sMMSpecialist2 = new SMMSpecialist("Вася", 31, 85, 370);
+        Employee sMMSpecialist = new SMMSpecialist("Костя", 24, 80, 350);
+        Employee sMMSpecialist1 = new SMMSpecialist("Петя", 29, 76, 330);
+        Employee sMMSpecialist2 = new SMMSpecialist("Вася", 31, 85, 370);
 
-     //  List<Employer> listOfSMMspecialist = new ArrayList<>();
-     //  listOfSMMspecialist.add(sMMSpecialist);
-     //  listOfSMMspecialist.add(sMMSpecialist1);
-     //  listOfSMMspecialist.add(sMMSpecialist2);
+        //  List<Employer> listOfSMMspecialist = new ArrayList<>();
+        //  listOfSMMspecialist.add(sMMSpecialist);
+        //  listOfSMMspecialist.add(sMMSpecialist1);
+        //  listOfSMMspecialist.add(sMMSpecialist2);
 
-     //  listOfSMMspecialist.toArray()
-        Employer[] employersArray = new Employer[] {sMMSpecialist, sMMSpecialist1, sMMSpecialist2};
+        //  listOfSMMspecialist.toArray()
+        Employee[] employersArray = new Employee[]{sMMSpecialist, sMMSpecialist1, sMMSpecialist2};
         Arrays.sort(employersArray);
         System.out.println(Arrays.toString(employersArray));
 
-        Employer ceo = new CEO("Илон", 48, 100, 1000000);
+        Employee ceo = new CEO("Илон", 48, 100, 1000000);
 
-        Employer loader = new Loader("Генадий", 42, 60, 150);
-        Employer loader1 = new Loader("Ибрагим", 37, 55, 140);
-        Employer loader2 = new Loader("Тимур", 25, 80, 170);
+        Employee loader = new Loader("Генадий", 42, 60, 150);
+        Employee loader1 = new Loader("Ибрагим", 37, 55, 140);
+        Employee loader2 = new Loader("Тимур", 25, 80, 170);
 
 
-        List<Employer> arraySalary = new ArrayList<>();
+        List<Employee> arraySalary = new ArrayList<>();
         arraySalary.add(cashier);
         arraySalary.add(cashier1);
         arraySalary.add(cashier2);
@@ -52,9 +56,35 @@ public class Main {
         CleanWasher cleanWasher2 = (CleanWasher) cleanWasher.clone();
         System.out.println(cleanWasher2.getAge());
 
+
+     //  String ceoCount = null;
+     //  String cleanWasherCount = null;
+     //  String cashierCount = null;
+
+     //  try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
+     //      System.out.println("Введите количество Сeo");
+     //      ceoCount = console.readLine();
+
+     //      System.out.println("Введите количество CleanWasher");
+
+     //      cleanWasherCount = console.readLine();
+
+     //      System.out.println("Введитье количество Cashier");
+
+     //      cashierCount = console.readLine();
+
+
+     //  } catch (IOException e) {
+     //      e.printStackTrace();
+     //  }
+     //  System.out.println("Количество CEO: " + ceoCount);
+     //  System.out.println("Количество уборщиц: " + cleanWasherCount);
+     //  System.out.println("Количество кассиров: " + cashierCount);
+
+
     }
 
-        }
+}
 
 //1. Иерархию сотрудников. Employer от него наследуються CleanWasher, Cashier, SMMSpecialist, CEO и кто-то еще.
 // имя, возраст, double power( вовлеченность) , salary. некоторые поля надо сделать final
